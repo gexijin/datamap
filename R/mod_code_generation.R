0cc307f9fde084daa6fcbd98244e07b62a9cea95
Steven Ge <xijin.ge@sdstate.edu>
2025-04-20 13:28:02 -0500

    Minor ui

--- R/mod_code_generation.R
+++ R/mod_code_generation.R
@@ -148,10 +148,10 @@ code_generation_server <- function(id, file_data, transform_data, heatmap_result
       tagList(
         css,
         tags$div(
-            fluidRow(
-            column(width = 4, style = "margin-top:5px;", downloadButton(ns("download_combined_code"), "Generated R Script")),
-            column(width = 8, style = "margin-top:5px;", tags$p("To reproduce the plots, save and run it in a directory that also contains all the data files.", style = "margin-left: 15px;"))
-            ),
+          fluidRow(
+            column(width = 8, style = "margin-top:5px;", tags$p("To reproduce the plots, save and run it in a directory that also contains all the data files.")),
+            column(width = 4, style = "margin-top:5px;", downloadButton(ns("download_combined_code"), "Generated R Script"))
+          ),
           html_output
         )
       )
